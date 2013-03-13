3972dbe22eeb1aa58e60877e201655f525b861b5
Tom Vogt <tom@lemuria.org>
2013-03-13 23:21:44 +0100

    and yet more work to make travis happy

--- Tests/Controller/WorkControllerTest.php
+++ Tests/Controller/WorkControllerTest.php
@@ -7,6 +7,33 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class WorkControllerTest extends WebTestCase {
 
-// TODO
-	
+	private $pages = array(
+		array('url' => '/en/translator/work', 'content'=>'TODO'),
+	);
+
+	private function login($client) {
+		$client->followRedirects();
+		$crawler = $client->request('GET', '/en/login');
+		$form = $crawler->selectButton('_submit')->form(array(
+			'_username'  => 'user',
+			'_password'  => 'user'
+			));		
+		$client->submit($form);
+	}
+
+
+
+	public function testBasics() {
+		$client = static::createClient();
+		$this->login($client);
+
+		foreach ($this->pages as $page) {
+			$crawler = $client->request('GET', $page['url']);
+			$this->assertTrue($client->getResponse()->isSuccessful(), "page ".$page['url']." failed to load");
+			$this->assertTrue($crawler->filter('html:contains("'.$page['content'].'")')->count() > 0, "page ".$page['url']." content failure");
+		}
+
+		$crawler = $client->request('GET', '/en/translator/work/doesntexist');
+		$this->assertFalse($client->getResponse()->isSuccessful(), "dummy fail page should not load");
+	}
 }
--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,8 +1,10 @@
 <?php
 
-$file = __DIR__.'/../vendor/autoload.php';
-if (!file_exists($file)) {
-    throw new RuntimeException('Install dependencies to run test suite. "php composer.phar install --dev"');
+if (file_exists($file = __DIR__.'/autoload.php')) {
+    require_once $file;
+} else {
+    require_once __DIR__.'/autoload.php.dist';
 }
 
-require_once $file;
+require_once __DIR__ . '/Functional/app/WebTestCase.php';
+require_once __DIR__ . '/Functional/app/TestController.php';
